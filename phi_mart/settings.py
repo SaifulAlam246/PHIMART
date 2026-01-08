71a8780a46fa131232ec636becde74b47caabec7
saifulalam <saifulalamsaimon85@gmail.com>
2026-01-08 19:36:26 +0600

    module-20 added

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -130,3 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'CORECE_DECIMAL_TO_STRING' : False
+}
---
+++ product/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from decimal import Decimal
+import product
+from product.models import Product,Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description', 'product_count']
+
+    product_count = serializers.IntegerField()
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'price',
+        'stock', 'category', 'price_with_tax'] 
+
+    price_with_tax = serializers.SerializerMethodField(
+    method_name='calculate_tax')
+
+    def calculate_tax(self, product):
+        return round(product.price * Decimal(1.1), 2)
