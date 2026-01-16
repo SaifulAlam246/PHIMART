f0b68f01bd76acc4f26015bdb3b603c7ef4fd8fa
saifulalam <saifulalamsaimon85@gmail.com>
2026-01-16 18:45:48 +0600

    advanced django concept

--- product/serializers.py
+++ product/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 from decimal import Decimal
 import product
-from product.models import Product,Category
+from product.models import Product,Category,Review
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'description', 'product_count']
 
-    product_count = serializers.IntegerField()
+    product_count = serializers.IntegerField(read_only=True)
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,3 +21,13 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def calculate_tax(self, product):
         return round(product.price * Decimal(1.1), 2)
+    
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'name', 'description']
+
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return Review.objects.create(product_id=product_id, **validated_data)    
--- product/views.py
+++ product/views.py
@@ -1,56 +1,36 @@
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from product.models import Product, Category
-from product.serializers import ProductSerializer,CategorySerializer
+from product.models import Product, Category,Review
+from product.serializers import ProductSerializer,CategorySerializer,ReviewSerializer
 from django.db.models import Count
+from rest_framework.viewsets import ModelViewSet
 # Create your views here.
 
-@api_view(['GET','POST'])
-def view_products(request):
-    if request.method=='GET':
-        products=Product.objects.select_related('category').all()
-        serializer=ProductSerializer(products,many=True)
-        return Response(serializer.data)
-    if request.method=='POST':
-        serializer=ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data,status=status.HTTP_201_CREATED)
+class ProductViewSet(ModelViewSet):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
 
-@api_view()
-def view_categories(request):
-    categories=Category.objects.annotate(product_count=Count('products')).all()
-    serializer=CategorySerializer(categories,many=True)
-    return Response(serializer.data)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def view_specific_product(request, id):
-    if request.method == 'GET':
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-    if request.method == 'PUT':
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    if request.method == 'DELETE':
-        product = get_object_or_404(Product, pk=id)
-        copy_of_product = product
-        product.delete()
-        serializer = ProductSerializer(copy_of_product)
-        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
-
-@api_view()
-def view_specific_category(request, pk):
-    category = get_object_or_404(Category, pk=pk)
-    serializer=CategorySerializer(category)
-    return Response(serializer.data)
-
+    def destroy(self,request,*args,**kwargs):
+       product=self.get_object()
+       if product.stock > 10 :
+           return Response({'message': "Eta delete kora jabe na."})
+       self.perform_destroy(product)
+       return Response (status=status.HTTP_204_NO_CONTENT)
+    
+class CategoryViewSet(ModelViewSet):
+    queryset=Category.objects.annotate(
+        product_count=Count('products')).all()
+    serializer_class=CategorySerializer
     
 
+class ReviewViewSet(ModelViewSet):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
 
